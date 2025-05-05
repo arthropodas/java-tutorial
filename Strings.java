6f9c85528bb670b19e2077e3f954cce4de309442
asifnp <asif.np@innovaturelabs.com>
2025-05-05 08:56:17 +0530

    created develop

--- Strings.java
+++
@@ -1,7 +0,0 @@
-public class Strings{
-    public static  void main(String aregs[]){
-        String name = "asif";
-        name = "overrided name";
-        System.err.println("cooccoo"+name);
-    }
-}
--- Wrapper.java
+++
@@ -1,7 +0,0 @@
-    public class Wrapper{
-        public static void main(String[] args) {
-            int num = 5;
-            Integer num1 = num;
-             
-        }
-    }
