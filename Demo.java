7ab755f10a36d51c2547024d0075d584d8641bb2
asifnp <asif.np@innovaturelabs.com>
2025-05-05 17:12:12 +0530

    added buffered reader

--- Demo.java
+++ Demo.java
@@ -1,4 +1,8 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class CustomException extends Exception {
 
     public CustomException(String s) {
@@ -8,15 +12,21 @@ class CustomException extends Exception {
 
 public class Demo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = null;
+
         try {
             String s = "new";
             System.out.println("System out print ln is " + s.length());
-            int j = 0;
+            int j = 1;
             if (j == 0) {
                 throw new CustomException("don't want denominator as zero");
             }
             System.out.println("result" + 18 / j);
+            InputStreamReader in = new InputStreamReader(System.in);
+            br = new BufferedReader(in);
+            int num = Integer.parseInt(br.readLine());
+            System.out.println("the number is " + num);
         } catch (NullPointerException e) {
 
         } catch (CustomException e) {
@@ -24,7 +34,7 @@ public class Demo {
         } catch (Exception e) {
             System.out.println("hi this is exception" + e);
         } finally {
-
+            br.close();
             System.out.println("After result");
 
         }
